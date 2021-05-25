d53d90e423078f93f2beff3087cda4ab868f24c9
Praveen R <rpraveen@vmware.com>
2021-05-26 00:10:22 +0530

    Adding Details page and css and route

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../screens/home/Home.js';
+import Details from '../screens/details/Details.js';
+
 
 class Controller extends Component {
 
@@ -12,8 +14,9 @@ class Controller extends Component {
     render() {
         return (
             <Router>
-                <div>
+                <div className="main-container">
                      <Route exact path="/" render={(props) => <Home {...props} baseUrl = {this.baseUrl} />} /> 
+                     <Route exact path="/movie/:id" render={(props) => <Details {...props} baseUrl = {this.baseUrl} />} /> 
                 </div>
             </Router>
         )
